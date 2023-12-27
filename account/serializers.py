79093477b6f140f8b43f400b2e8e6da894c9479c
KrutikaParab14 <55806064+krutikaParab@users.noreply.github.com>
2023-12-28 01:10:52 +0530

    Add files via upload

--- account/serializers.py
+++ account/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.contrib.auth import authenticate
 
 class RegisterSerializer(serializers.Serializer):
     first_name = serializers.CharField()
@@ -17,7 +19,34 @@ class RegisterSerializer(serializers.Serializer):
     def create(self, validated_data):
         user = User.objects.create(first_name=validated_data['first_name'],
                                    last_name=validated_data['last_name'],
-                                   username=validated_data['username'])
+                                   username=validated_data['username'].lower())
         user.set_password(validated_data['password'])
 
         return validated_data
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField()
+
+    def validate(self, data):
+        if not User.objects.filter(username=data['username']).exists():
+            raise serializers.ValidationError('Account Not Found')
+        return data
+
+    def get_jwt_token(self, data):
+
+        user = authenticate(username=data['username'], password=data['password'])
+        if not user:
+            return {
+                'message': "Invalid Credentials",
+                'data': {}
+            }
+
+        refresh = RefreshToken.for_user(user)
+        return {
+            'message': 'Login Success',
+            'data': {'token':
+            {
+            'refresh': str(refresh),
+            'access': str(refresh.access_token),
+        }}}
--- account/views.py
+++ account/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from .serializers import RegisterSerializer
+from .serializers import RegisterSerializer, LoginSerializer
 from rest_framework import status
 
 
@@ -40,3 +40,29 @@ class RegisterView(APIView):
                 },
                 status=status.HTTP_400_BAD_REQUEST
             )
+
+class LoginView(APIView):
+    def post(self, request):
+        try:
+            data = request.data
+            serializer = LoginSerializer(data=data)
+
+            if not serializer.is_valid():
+                return Response({
+                    'data': serializer.errors,
+                    'message': 'Something went wrong'
+                },  status=status.HTTP_400_BAD_REQUEST)
+
+            response = serializer.get_jwt_token(data=serializer.data)
+
+            return Response({
+                    'data': response,
+                    'message': 'login '
+                }, status=status.HTTP_200_OK)
+
+        except Exception as e:
+            print(e)
+            return Response({
+                'data': {},
+                'message': "Something went wrong"
+            }, status=status.HTTP_400_BAD_REQUEST)
